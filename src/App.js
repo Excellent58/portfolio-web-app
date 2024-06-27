adfe8fb1d33c2d5071bc8177b08fe7a1bd60f342
Excellent58 <wycliffemusau99@gmail.com>
2024-06-27 17:59:07 +0300

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,57 +1,14 @@
-import { useState, useRef, useEffect } from 'react';
 import './App.css';
-import anime from "animejs/lib/anime.es.js"
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Player/>
+      <NavBar/>
     </div>
   );
 }
 
-const ticks = Array.from(Array(8));
 
-const Player = () => {
-  const [playing, setPlaying] = useState(false);
-
-  const animation = useRef(null)
-
-  useEffect(() => {
-    animation.current = anime.timeline({
-      direction: "alternate",
-      loop: true,
-      autoplay: false,
-      easing: "easeInOutSine"
-    });
-
-    for (const tick in ticks) {
-      animation.current.add(
-        {
-          targets: `.dots li:nth-child(${Number(tick) + 1})`,
-          scaleY: 1.5 + Math.random() * 4,
-          duration: 300 + Math.random() * 300
-        },
-        Math.random() * 600
-      );
-    }
-  }, []);
-
-  const handleClick = () => {
-    playing ? animation.current.pause() : animation.current.play();
-    setPlaying(!playing)
-  }
-
-  return (
-    <div className='player'>
-      <ul className='dots'>
-        {ticks.map((_, i) => (
-          <li key={i} />
-        ))}
-      </ul>
-       <button onClick={handleClick}>{playing ? "Pause" : "Play"}</button>
-    </div>
-  )
-}
 
 export default App;
