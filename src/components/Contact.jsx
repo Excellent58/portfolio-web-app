784433b8f6f8297d19b060ad1191a16ad6804d78
Excellent58 <wycliffemusau99@gmail.com>
2024-08-27 19:26:03 +0300

    fixed tect section ui

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,13 +4,12 @@ export default function Contact() {
   return (
     <section className="m-6 mt-28" id="contact">
       <h2 className="font-bold text-lg text-blue-400">Contact</h2>
-      <p className="text-blue-300 mt-5">Work with me....</p>
 
       <div className="text-center">
-        <h1 className="text-xl md:text-2xl font-bold text-blue-200">Get In Touch</h1>
+        <h1 className="text-xl md:text-2xl font-bold text-blue-400">Get In Touch</h1>
         <p>
           I am open for a remote job, freelance work or any new opportunities.<br/>
-          If you have any questions or just want to say hi, feel free to reach out to me.
+          Feel free to get in touch and talk more about your projects.
         </p>
         <div className="text-blue-400">
           <div className="mt-6 flex justify-center space-x-4">
--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -20,7 +20,7 @@ export default function TechStack() {
     <section className="m-6 mt-28" id="techstack">
       <h2 className="font-bold text-lg text-blue-400">TechStack</h2>
       <div className="mt-5 flex flex-col md:flex-row md:justify-between md:items-center">
-        <div className="grid grid-cols-4 gap-5 sm:gap-10 md:gap-16 md:grid-cols-6 lg:grid-cols-7">
+        <div className="grid grid-cols-4 gap-3 sm:gap-4 md:gap-10 md:grid-cols-6 lg:grid-cols-7">
           <img src={PythonIcon} alt="python Logo" className="w-10 h-10"/>
 
           <img src={JsIcon} alt="HTML Logo" className="w-10 h-10"/>
@@ -50,14 +50,41 @@ export default function TechStack() {
           <img src={GitIcon} alt="git Logo" className="w-10 h-10"/>
         </div>
 
-        <div className="image">
+        <div className="flex md:space-x-10 flex-col md:flex-row items-center md:items-start">
+          <div className="space-y-3">
+            <p className="border text-blue-400 border-slate-900 flex items-center justify-center w-20 rounded-xl bg-slate-900">Python</p>
+            <p className="border text-blue-400 border-slate-900 flex items-center justify-center w-20 rounded-xl bg-slate-900">Javascript</p>
+            <p className="border text-blue-400 border-slate-900 flex items-center justify-center w-20 rounded-xl bg-slate-900">Typescript</p>
+            <p className="border text-blue-400 border-slate-900 flex items-center justify-center w-20 rounded-xl bg-slate-900">Golang</p>
+          </div>
+
+          <div className="space-y-3">
+            <p className="border text-blue-400 border-slate-900 flex items-center justify-center w-20 rounded-xl bg-slate-900">React</p>
+            <p className="border text-blue-400 border-slate-900 flex items-center justify-center w-20 rounded-xl bg-slate-900">Tailwindcss</p>
+            <p className="border text-blue-400 border-slate-900 flex items-center justify-center w-20 rounded-xl bg-slate-900">Nextjs</p>
+            <p className="border text-blue-400 border-slate-900 flex items-center justify-center w-20 rounded-xl bg-slate-900">django</p>
+            <p className="border text-blue-400 border-slate-900 flex items-center justify-center w-20 rounded-xl bg-slate-900">Flask</p>
+            <p className="border text-blue-400 border-slate-900 flex items-center justify-center w-20 rounded-xl bg-slate-900">Gin</p>
+            <p className="border text-blue-400 border-slate-900 flex items-center justify-center w-20 rounded-xl bg-slate-900">Fiber</p>
+          </div>
+
+          <div className="space-y-3">
+            <p className="border text-blue-400 border-slate-900 flex items-center justify-center w-20 rounded-xl bg-slate-900">Postgresql</p>
+            <p className="border text-blue-400 border-slate-900 flex items-center justify-center w-20 rounded-xl bg-slate-900">Msql</p>
+            <p className="border text-blue-400 border-slate-900 flex items-center justify-center w-20 rounded-xl bg-slate-900">Sqlite3</p>
+            <p className="border text-blue-400 border-slate-900 flex items-center justify-center w-20 rounded-xl bg-slate-900">MongoDB</p>
+            <p className="border text-blue-400 border-slate-900 flex items-center justify-center w-20 rounded-xl bg-slate-900">Redis</p>
+          </div>
+        </div>
+
+        {/* <div className="image">
           <img
             src={TechStackImage}
-            height={600}
-            width={500}
+            height={500}
+            width={400}
             alt="about"
           />
-        </div>
+        </div> */}
       </div>
     </section>
   )
